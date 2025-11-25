d4b8248e640392885f7f41f28d8d8e5c0d055846
rlaals0203 <scientistmin@gmail.com>
2025-11-25 12:11:25 +0900

    fix : pattern

--- 2025_winapi_framework_21/TestBoss.cpp
+++ 2025_winapi_framework_21/TestBoss.cpp
@@ -22,6 +22,8 @@ TestBoss::TestBoss()
 	AddModule(new CircleProjectilePattern(m_Controller, ProjectileType::Enemy, 40));
 	AddModule(new MovePattern(m_Controller, { WINDOW_WIDTH * 0.20f, WINDOW_HEIGHT * 0.20f }, 300.f, 0.5f));
 	AddModule(new MovePattern(m_Controller, { WINDOW_WIDTH * 0.30f, WINDOW_HEIGHT * 0.45f }, 300.f, 0.5f));
+	AddModule(new GuidedProjectilePattern(m_Controller, ProjectileType::Enemy, 0.5f, 10));
+
 	auto* col = AddComponent<Collider>();
 	col->SetSize({ 125, 125 });
 	SetSize({ 250, 250 });
