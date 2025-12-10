aed54fabc572f8af4385cb9e893def49a4af18a9
kimdonghyun81222 <157779631+kimdonghyun81222@users.noreply.github.com>
2025-12-11 08:56:43 +0900

    fix : memory leak

--- 2025_winapi_framework_21/Scene.cpp
+++ 2025_winapi_framework_21/Scene.cpp
@@ -3,6 +3,7 @@
 #include "Object.h"
 #include "CollisionManager.h"
 #include "Rigidbody.h"
+
 void Scene::FixedUpdate(float _fixedDT)
 {
 	for (UINT i = 0; i < (UINT)Layer::END; ++i)
@@ -10,14 +11,18 @@ void Scene::FixedUpdate(float _fixedDT)
 		auto& vec = m_vecObj[i];
 		for (auto* obj : vec)
 		{
-			if (auto rb = obj->GetComponent<Rigidbody>())
+			if (obj && !obj->GetIsDead())
 			{
-				if (rb != nullptr)
-					rb->FixedUpdate(_fixedDT);
+				if (auto rb = obj->GetComponent<Rigidbody>())
+				{
+					if (rb != nullptr)
+						rb->FixedUpdate(_fixedDT);
+				}
 			}
 		}
 	}
 }
+
 Scene::Scene()
 {
 }
@@ -29,23 +34,39 @@ Scene::~Scene()
 
 void Scene::Update()
 {
-	// ?? OBJ UPDATE
 	for (UINT i = 0; i < (UINT)Layer::END; ++i)
 	{
 		auto& vec = m_vecObj[i];
 		for (auto* obj : vec)
-			if(!obj->GetIsDead())
+		{
+			if (!obj->GetIsDead())
 				obj->Update();
+		}
 	}
 }
+
 void Scene::LateUpdate()
 {
 	for (UINT i = 0; i < (UINT)Layer::END; ++i)
 	{
 		auto& vec = m_vecObj[i];
-		for (auto* obj : vec)
-			if (!obj->GetIsDead())
-				obj->LateUpdate();
+
+		auto iter = vec.begin();
+		while (iter != vec.end())
+		{
+			if ((*iter)->GetIsDead())
+			{
+				delete (*iter);
+				(*iter) = nullptr;
+
+				iter = vec.erase(iter);
+			}
+			else
+			{
+				(*iter)->LateUpdate();
+				++iter;
+			}
+		}
 	}
 }
 
@@ -55,19 +76,25 @@ void Scene::Render(HDC _hdc)
 	{
 		auto& vec = m_vecObj[i];
 		for (auto* obj : vec)
+		{
 			if (!obj->GetIsDead())
 				obj->Render(_hdc);
+		}
 	}
 }
 
 void Scene::Release()
 {
 	FlushEvent();
+
 	for (UINT i = 0; i < (UINT)Layer::END; ++i)
 	{
 		auto& vec = m_vecObj[i];
 		for (auto* obj : vec)
-			SAFE_DELETE(obj);
+		{
+			if (obj)
+				delete obj;
+		}
 		vec.clear();
 	}
 	GET_SINGLE(CollisionManager)->CheckReset();
@@ -78,14 +105,12 @@ void Scene::RequestDestroy(Object* obj)
 	if (obj == nullptr)
 		return;
 
-	// 죽지 않으면 넣어라
 	if (!obj->GetIsDead())
 	{
 		obj->SetDead();
-		// 실제 delete는 FlushObject에서
-		m_killObject.push_back(obj);
 	}
 }
+
 void Scene::FlushEvent()
 {
 	for (auto& spawn : m_spawnObject)
@@ -93,13 +118,6 @@ void Scene::FlushEvent()
 		AddObject(spawn.obj, spawn.type);
 	}
 	m_spawnObject.clear();
-
-	for (Object* d : m_killObject)
-	{
-		RemoveObject(d);
-		SAFE_DELETE(d);
-	}
-	m_killObject.clear();
 }
 
 void Scene::ClearScene()
@@ -108,8 +126,10 @@ void Scene::ClearScene()
 	{
 		auto& vec = m_vecObj[i];
 		for (auto* obj : vec)
+		{
 			if (obj->IsDestroyOnComplete())
 				obj->SetDead();
+		}
 	}
 }
 
@@ -121,12 +141,16 @@ void Scene::RequestSpawn(Object* _obj, Layer _type)
 	m_spawnObject.push_back(newObj);
 }
 
-
 void Scene::RemoveObject(Object* _obj)
 {
 	for (UINT i = 0; i < (UINT)Layer::END; ++i)
 	{
 		auto& v = m_vecObj[i];
-		v.erase(std::remove(v.begin(), v.end(), _obj), v.end());
+		auto iter = std::find(v.begin(), v.end(), _obj);
+		if (iter != v.end())
+		{
+			v.erase(iter);
+			return;
+		}
 	}
 }
